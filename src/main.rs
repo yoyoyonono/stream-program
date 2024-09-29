97a8da8da2e2f333a96b76593f15b11a669e9e7f
Aaron Thapa <notsmart98@gmail.com>
2024-09-29 19:32:21 -0400

    format

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-use std::sync::{Arc, Mutex};
-use std::{thread, time::Duration};
-use queues::*;
-use tokio::{task, time};
-use tts::Tts;
 use brainrot::{twitch, youtube};
-use futures_util::StreamExt;
 use chrono;
+use futures_util::StreamExt;
 use iced;
 use iced::widget;
+use queues::*;
+use std::sync::{Arc, Mutex};
+use std::{thread, time::Duration};
+use tokio::{task, time};
+use tts::Tts;
 
 #[derive(Debug, Clone)]
 struct ChatMessage {
     author: String,
-    text: String
+    text: String,
 }
 
 #[derive(Default)]
@@ -29,7 +29,7 @@ impl AppState {
             AppMessage::Decrement => {
                 self.value -= 1;
             }
-        }        
+        }
     }
 
     fn view(&self) -> widget::Column<AppMessage> {
@@ -37,7 +37,8 @@ impl AppState {
             widget::button("Increment").on_press(AppMessage::Increment),
             widget::text(self.value).size(50),
             widget::button("Decrement").on_press(AppMessage::Decrement)
-        ].padding(20)
+        ]
+        .padding(20)
         .align_x(iced::Center)
     }
 }
@@ -45,14 +46,14 @@ impl AppState {
 #[derive(Debug, Clone, Copy)]
 enum AppMessage {
     Increment,
-    Decrement
+    Decrement,
 }
 
 #[tokio::main]
 async fn main() {
     let yt_channel_id = "UCl8NSZ2GyhoQlAnZmW4cpBw";
     let twitch_id = "yoyoyonono";
-    
+
     let mut tts_voice = Tts::new(tts::Backends::WinRt).unwrap();
     tts_voice.speak("hello, world", false).unwrap();
     thread::sleep(Duration::from_secs(2));
@@ -65,17 +66,35 @@ async fn main() {
     let program_start_time = chrono::Utc::now();
 
     let yt_handler = tokio::spawn(async move {
-        if let Result::Ok(context) = youtube::ChatContext::new_from_channel(yt_channel_id, youtube::ChannelSearchOptions::LatestLiveOrUpcoming).await {
+        if let Result::Ok(context) = youtube::ChatContext::new_from_channel(
+            yt_channel_id,
+            youtube::ChannelSearchOptions::LatestLiveOrUpcoming,
+        )
+        .await
+        {
             let mut stream = youtube::stream(&context).await.unwrap();
             println!("Youtube connected");
             while let Some(Ok(c)) = stream.next().await {
-                if let youtube::Action::AddChatItem { item: youtube::ChatItem::TextMessage {message_renderer_base, message}, .. } = c {
+                if let youtube::Action::AddChatItem {
+                    item:
+                        youtube::ChatItem::TextMessage {
+                            message_renderer_base,
+                            message,
+                        },
+                    ..
+                } = c
+                {
                     if message_renderer_base.timestamp_usec < program_start_time {
                         continue;
                     }
                     let text = ChatMessage {
                         author: message_renderer_base.author_name.unwrap().simple_text,
-                        text: message.unwrap().runs.into_iter().map(|c| c.to_chat_string()).collect::<String>()
+                        text: message
+                            .unwrap()
+                            .runs
+                            .into_iter()
+                            .map(|c| c.to_chat_string())
+                            .collect::<String>(),
                     };
                     let mut lock = tts_queue_yt.lock().unwrap();
                     lock.add(text).unwrap();
@@ -86,15 +105,17 @@ async fn main() {
     });
 
     let twitch_handler = tokio::spawn(async move {
-        let mut client = brainrot::TwitchChat::new(twitch_id, twitch::Anonymous).await.unwrap();
+        let mut client = brainrot::TwitchChat::new(twitch_id, twitch::Anonymous)
+            .await
+            .unwrap();
 
         println!("Twitch connected");
 
         while let Some(message) = client.next().await.transpose().unwrap() {
-            if let brainrot::TwitchChatEvent::Message {user, contents, ..} = message {
+            if let brainrot::TwitchChatEvent::Message { user, contents, .. } = message {
                 let text = ChatMessage {
                     author: user.display_name,
-                    text: contents.iter().map(|c| c.to_string()).collect::<String>()
+                    text: contents.iter().map(|c| c.to_string()).collect::<String>(),
                 };
                 let mut lock = tts_queue_twitch.lock().unwrap();
                 lock.add(text).unwrap();
@@ -103,23 +124,20 @@ async fn main() {
         }
     });
 
-
-    thread::spawn(move || {
-        loop {
-            let mut lock = tts_queue_arc.lock().unwrap();
-            println!("{:?}", lock);
-            if lock.size() > 0 {
-                let text = lock.remove().unwrap();
-                tts_voice.speak(format!("{}, {}", &text.author, &text.text), false).unwrap();
-            }
-            drop(lock);
-            thread::sleep(Duration::from_millis(100));
+    thread::spawn(move || loop {
+        let mut lock = tts_queue_arc.lock().unwrap();
+        println!("{:?}", lock);
+        if lock.size() > 0 {
+            let text = lock.remove().unwrap();
+            tts_voice
+                .speak(format!("{}, {}", &text.author, &text.text), false)
+                .unwrap();
         }
+        drop(lock);
+        thread::sleep(Duration::from_millis(100));
     });
 
     yt_handler.await.unwrap();
     twitch_handler.await.unwrap();
     iced::run("A cool counter", AppState::update, AppState::view);
-
 }
-
