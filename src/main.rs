4741b4dd5ff368db9d1f74895fca9088128714c6
Aaron Thapa <notsmart98@gmail.com>
2025-05-11 03:12:59 -0400

    working danmaku

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use brainrot::{twitch, youtube};
 use chrono;
 use eframe::egui;
+use egui::{ecolor, Align2, Color32, FontId, Pos2, Rect};
 use futures_util::StreamExt;
 use queues::*;
+use rand::Rng;
 use std::sync::{Arc, Mutex};
+use std::time::Instant;
 use std::{thread, time::Duration};
-use tokio::{task, time};
 use tts::Tts;
 
 #[derive(Debug, Clone)]
@@ -20,7 +22,7 @@ async fn main() {
     let twitch_id = "yoyoyonono";
 
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([1280.0, 720.0]),
+        viewport: egui::ViewportBuilder::default().with_inner_size([300.0, 720.0]),
         ..Default::default()
     };
 
@@ -37,6 +39,12 @@ async fn main() {
     let tts_queue_arc = Arc::new(Mutex::new(tts_queue));
     let tts_queue_yt = Arc::clone(&tts_queue_arc);
     let tts_queue_twitch = Arc::clone(&tts_queue_arc);
+
+    let danmaku_queue = queue![];
+    let danmaku_queue_arc = Arc::new(Mutex::new(danmaku_queue));
+    let danmaku_queue_yt = Arc::clone(&danmaku_queue_arc);
+    let danmaku_queue_twitch = Arc::clone(&danmaku_queue_arc);
+    let danmaku_queue_window = Arc::clone(&danmaku_queue_arc);
 
     let program_start_time = chrono::Utc::now();
 
@@ -73,6 +81,10 @@ async fn main() {
                 let mut lock = tts_queue_yt.lock().unwrap();
                 lock.add(message_1).unwrap();
                 drop(lock);
+
+                let mut danmaku_lock = danmaku_queue_yt.lock().unwrap();
+                danmaku_lock.add(message_text.clone()).unwrap();
+                drop(danmaku_lock);
             }
         }
     });
@@ -95,34 +107,136 @@ async fn main() {
                 let mut lock = tts_queue_twitch.lock().unwrap();
                 lock.add(message_1).unwrap();
                 drop(lock);
+
+                let mut danmaku_lock = danmaku_queue_twitch.lock().unwrap();
+                danmaku_lock.add(message_text.clone()).unwrap();
             }
         }
     });
 
     thread::spawn(move || loop {
-        let mut lock = tts_queue_arc.lock().unwrap();
-        println!("{:?}", lock);
-        if lock.size() > 0 {
-            let text = lock.remove().unwrap();
+        let mut queue_lock = tts_queue_arc.lock().unwrap();
+        if queue_lock.size() > 0 {
+            let text = queue_lock.remove().unwrap();
+            let mut lock_text = chat_history_add.lock().unwrap();
+            *lock_text = format!("\n{}: {}", &text.author, &text.text) + &lock_text;
             tts_voice
                 .speak(format!("{}, {}", &text.author, &text.text), false)
                 .unwrap();
-            let mut lock_text = chat_history_add.lock().unwrap();
-            *lock_text += &format!("{}: {}\n", &text.author, &text.text);
         }
-        drop(lock);
+        drop(queue_lock);
         thread::sleep(Duration::from_millis(100));
     });
 
-    let _ = eframe::run_simple_native("My egui App", options, move |ctx, _frame| {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.add(
-                egui::TextEdit::multiline(&mut chat_history_remove.lock().unwrap().clone())
-                    .desired_rows(40),
-            );
-        });
-    });
+    eframe::run_native(
+        "Chat window",
+        options,
+        Box::new(|cc| {
+            Ok(Box::<AppState>::new(AppState::new(
+                chat_history_remove,
+                danmaku_queue_window,
+            )))
+        }),
+    )
+    .unwrap();
 
     yt_handler.await.unwrap();
     twitch_handler.await.unwrap();
+}
+
+struct AppState {
+    chat: Arc<Mutex<String>>,
+    current_danmaku: Vec<DanmakuMessage>,
+    danmaku_queue: Arc<Mutex<Queue<String>>>,
+    last_frame_time: Instant,
+}
+
+impl AppState {
+    fn new(chat: Arc<Mutex<String>>, danmaku_queue: Arc<Mutex<Queue<String>>>) -> AppState {
+        AppState {
+            chat: chat,
+            current_danmaku: vec![],
+            danmaku_queue: danmaku_queue,
+            last_frame_time: Instant::now(),
+        }
+    }
+}
+
+impl eframe::App for AppState {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.add(
+                egui::TextEdit::multiline(&mut self.chat.lock().unwrap().clone()).desired_rows(40),
+            );
+        });
+        ctx.show_viewport_immediate(
+            egui::ViewportId::from_hash_of("viewport"),
+            egui::ViewportBuilder::default()
+                .with_title("Immediate viewport")
+                .with_inner_size([1920.0, 1080.0])
+                .with_resizable(false),
+            |ctx, class| {
+                egui::CentralPanel::default().show(ctx, |ui| {
+                    let painter = ui.painter();
+
+                    let mut danmaku_lock = self.danmaku_queue.lock().unwrap();
+                    while let Ok(message) = danmaku_lock.remove() {
+                        let mut y_pos = 0;
+                        while self.current_danmaku.iter().any(|d| (d.position[1] == y_pos as f32 && d.position[0] + d.width + 50.0 > 1920.0)) {
+                            y_pos += 30;
+                        }
+
+                        let color = ecolor::rgb_from_hsv([rand::rng().random_range(0.0..1.0), 0.5, 1.0].into());
+                        let converted_color = Color32::from_rgb(
+                                (color[0] * 255.0) as u8, 
+                                (color[1] * 255.0) as u8, 
+                                (color[2] * 255.0) as u8,
+                            );
+                        let text_width = painter.text(
+                            Pos2::new(0.0, 0.0),
+                            Align2::LEFT_TOP,
+                            &message,
+                            FontId::proportional(30.0),
+                            converted_color,
+                        ).width();
+                        self.current_danmaku.push(DanmakuMessage {
+                            content: message,
+                            position: [1920.0, y_pos as f32],
+                            color: converted_color,
+                            width: text_width,
+                        });
+                    }
+
+                    let move_distance = 100.0
+                        * Instant::now()
+                            .duration_since(self.last_frame_time)
+                            .as_secs_f32();
+                    self.last_frame_time = Instant::now();
+                    for danmaku in &mut self.current_danmaku {
+                        let pos = Pos2::new(danmaku.position[0] as f32, danmaku.position[1] as f32);
+                        painter.text(
+                            pos,
+                            Align2::LEFT_TOP,
+                            &danmaku.content,
+                            FontId::proportional(30.0),
+                            danmaku.color, 
+                        );
+                        danmaku.position[0] -= move_distance;
+                    }
+
+                    self.current_danmaku.retain(|danmaku| {
+                        danmaku.position[0] > 0.0 - danmaku.width
+                    });
+                });
+            },
+        );
+        ctx.request_repaint_after(Duration::from_millis(25));
+    }
+}
+
+struct DanmakuMessage {
+    content: String,
+    position: [f32; 2],
+    color: Color32,
+    width: f32,
 }
